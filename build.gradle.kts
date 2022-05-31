c0f87ca8a5df6e975a495a535259d8a8c9b86a26
Jorge <jmurillo@bcp.com.pe>
2022-05-31 10:54:26 -0500

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,23 +8,14 @@ plugins {
 
 catalog {
     versionCatalog {
-        // create("mbbk") {
-            from(files("mbbk.versions.toml"))
-        // }
-        // library("my-lib", "com.mycompany:mylib:1.2")
+        from(files("mbbk.versions.toml"))
     }
 }
 
 publishing {
     publications {
         create<MavenPublication>("mbbk") {
-            // println("Version Catalogs:")
-            // versionCatalogs.catalogNames.forEach { println(it) }
-            // println("-----------")
-            // println(versionCatalogs)
-            // println("-----------")
              from(components["versionCatalog"])
-            // from(versionCatalogs)
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,2 @@
 rootProject.name = "gradle-version-catalogs"
-
 enableFeaturePreview("VERSION_CATALOGS")
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("mbbk") {
-            from(files("mbbk.versions.toml"))
-        }
-    }
-}
