5a3a32df2e43f0f39f7662330210c97844c78e2b
Jorge <jmurillo@bcp.com.pe>
2022-09-15 14:28:43 -0500

    settings coherent

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,29 @@
 group = "com.bcp.bank"
-version = "1.0.1.lts"
+version = "defined-by-git-tag"
 
 plugins {
     `version-catalog`
     `maven-publish`
 }
 
+/// order of the files matter: lower index overrides definitions on later entries
+val featureFiles = listOf(
+    "dependencies/MBBKST-Maps/mbbkst-maps.toml",
+)
+val defaultFiles = listOf(
+    "dependencies/settings/mbbk.plugins.settings.toml",
+    "dependencies/settings/mbbk.bundles.settings.toml",
+    "dependencies/settings/mbbk.libraries.settings.toml",
+    "dependencies/settings/mbbk.versions.settings.toml",
+//    "dependencies/core/mbbk.versions.core.toml",
+//    "dependencies/core/mbbk.libraries.core.toml",
+)
+
 catalog {
     versionCatalog {
-        from(files("mbbk.versions.toml"))
+        from(files(
+            listOf(featureFiles, defaultFiles).flatten()
+        ))
     }
 }
 
